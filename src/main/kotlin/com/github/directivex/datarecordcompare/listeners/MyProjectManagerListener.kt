0bad3122c455eacc396624d17636015473be22ef
GitHub Action <action@github.com>
2022-12-16 06:06:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/directivex/datarecordcompare/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/directivex/datarecordcompare/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.directivex.datarecordcompare.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.directivex.datarecordcompare.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/directivex/datarecordcompare/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/directivex/datarecordcompare/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.directivex.datarecordcompare.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.directivex.datarecordcompare.MyBundle
 
 class MyApplicationService {
 
